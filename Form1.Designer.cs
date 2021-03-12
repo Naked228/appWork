3a8d72edcb98566f84f89d2aa8f28259d3fc70ab
Naked228 <57211122+Naked228@users.noreply.github.com>
2021-03-12 15:27:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace appWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnCopy = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.butLIST1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.butLIST2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходAltF4ToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходAltF4ToolStripMenuItem
+            // 
+            this.выходAltF4ToolStripMenuItem.Name = "выходAltF4ToolStripMenuItem";
+            this.выходAltF4ToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.выходAltF4ToolStripMenuItem.Text = "Выход Alt+F4";
+            this.выходAltF4ToolStripMenuItem.Click += new System.EventHandler(this.выходAltF4ToolStripMenuItem_Click);
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.Location = new System.Drawing.Point(254, 53);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.Size = new System.Drawing.Size(75, 23);
+            this.BtnCopy.TabIndex = 1;
+            this.BtnCopy.Text = "button1";
+            this.BtnCopy.UseVisualStyleBackColor = true;
+            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // butLIST1
+            // 
+            this.butLIST1.Location = new System.Drawing.Point(12, 117);
+            this.butLIST1.Name = "butLIST1";
+            this.butLIST1.Size = new System.Drawing.Size(75, 23);
+            this.butLIST1.TabIndex = 3;
+            this.butLIST1.Text = "LIST1";
+            this.butLIST1.UseVisualStyleBackColor = true;
+            this.butLIST1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(421, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // butLIST2
+            // 
+            this.butLIST2.Location = new System.Drawing.Point(12, 146);
+            this.butLIST2.Name = "butLIST2";
+            this.butLIST2.Size = new System.Drawing.Size(75, 23);
+            this.butLIST2.TabIndex = 6;
+            this.butLIST2.Text = "LIST2";
+            this.butLIST2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 268);
+            this.Controls.Add(this.butLIST2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.butLIST1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходAltF4ToolStripMenuItem;
+        private System.Windows.Forms.Button BtnCopy;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button butLIST1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button butLIST2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace appWork
+{
+    public partial class Form1 : Form
+    {
+        Excel.Workbook XLBook;
+        Excel.Worksheet XLLIST;
+        Excel.Range range;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходAltF4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e)
+        {
+            string fileName = @"D:\test\CopyMe.txt";
+            string PathPaste = @"D:\test\paste\CopyMe.txt";
+
+            System.IO.File.Copy(fileName, PathPaste);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\Dor\object\object.mdb";
+            StreamWriter sw = new StreamWriter(path);
+            //StreamWriter sw = new StreamWriter("test.txt", true, System.Text.Encoding.Default);
+            Console.WriteLine(textBox1.Text);
+            Console.WriteLine(path);
+            sw.WriteLine(textBox1.Text);
+            sw.Close();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string path = @"D:\Dor\XL\tester.xlsx";
+            //StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
+            //string a = sr.ReadToEnd();
+            //Console.WriteLine(a);
+            //richTextBox1.Text = a;
+            //sr.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
