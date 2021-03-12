ca75e868e645b45bca77cb51cb7516d43301022c
rmk <53907711+romashkadurashka@users.noreply.github.com>
2021-03-12 20:08:17 +0300

    rework.start
    beta branch

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,12 +32,10 @@ namespace appWork
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выходAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.BtnCopy = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.butLIST1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.butLIST2 = new System.Windows.Forms.Button();
+            this.btnDBPath = new System.Windows.Forms.Button();
+            this.btnXLPath = new System.Windows.Forms.Button();
+            this.lblDBPath = new System.Windows.Forms.Label();
+            this.lblXLPath = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,12 +48,12 @@ namespace appWork
             this.menuStrip1.Size = new System.Drawing.Size(533, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // файлToolStripMenuItem
             // 
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.выходAltF4ToolStripMenuItem});
+            this.файлToolStripMenuItem.Enabled = false;
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
             this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.файлToolStripMenuItem.Text = "Файл";
@@ -63,82 +61,64 @@ namespace appWork
             // выходAltF4ToolStripMenuItem
             // 
             this.выходAltF4ToolStripMenuItem.Name = "выходAltF4ToolStripMenuItem";
-            this.выходAltF4ToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
-            this.выходAltF4ToolStripMenuItem.Text = "Выход Alt+F4";
+            this.выходAltF4ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходAltF4ToolStripMenuItem.Text = "Выход";
             this.выходAltF4ToolStripMenuItem.Click += new System.EventHandler(this.выходAltF4ToolStripMenuItem_Click);
             // 
-            // BtnCopy
+            // btnDBPath
             // 
-            this.BtnCopy.Location = new System.Drawing.Point(254, 53);
-            this.BtnCopy.Name = "BtnCopy";
-            this.BtnCopy.Size = new System.Drawing.Size(75, 23);
-            this.BtnCopy.TabIndex = 1;
-            this.BtnCopy.Text = "button1";
-            this.BtnCopy.UseVisualStyleBackColor = true;
-            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            this.btnDBPath.Location = new System.Drawing.Point(13, 28);
+            this.btnDBPath.Name = "btnDBPath";
+            this.btnDBPath.Size = new System.Drawing.Size(113, 23);
+            this.btnDBPath.TabIndex = 1;
+            this.btnDBPath.Text = "БД";
+            this.btnDBPath.UseVisualStyleBackColor = true;
+            this.btnDBPath.Click += new System.EventHandler(this.btnDBPath_Click);
             // 
-            // textBox1
+            // btnXLPath
             // 
-            this.textBox1.Location = new System.Drawing.Point(254, 27);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.btnXLPath.Location = new System.Drawing.Point(13, 58);
+            this.btnXLPath.Name = "btnXLPath";
+            this.btnXLPath.Size = new System.Drawing.Size(113, 23);
+            this.btnXLPath.TabIndex = 2;
+            this.btnXLPath.Text = "Доработка (XL)";
+            this.btnXLPath.UseVisualStyleBackColor = true;
+            this.btnXLPath.Click += new System.EventHandler(this.btnXLPath_Click);
             // 
-            // butLIST1
+            // lblDBPath
             // 
-            this.butLIST1.Location = new System.Drawing.Point(12, 117);
-            this.butLIST1.Name = "butLIST1";
-            this.butLIST1.Size = new System.Drawing.Size(75, 23);
-            this.butLIST1.TabIndex = 3;
-            this.butLIST1.Text = "LIST1";
-            this.butLIST1.UseVisualStyleBackColor = true;
-            this.butLIST1.Click += new System.EventHandler(this.button2_Click);
+            this.lblDBPath.AutoSize = true;
+            this.lblDBPath.Location = new System.Drawing.Point(132, 33);
+            this.lblDBPath.Name = "lblDBPath";
+            this.lblDBPath.Size = new System.Drawing.Size(115, 13);
+            this.lblDBPath.TabIndex = 3;
+            this.lblDBPath.Text = "Путь к базе данных";
             // 
-            // button2
+            // lblXLPath
             // 
-            this.button2.Location = new System.Drawing.Point(340, 53);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(421, 27);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
-            // 
-            // butLIST2
-            // 
-            this.butLIST2.Location = new System.Drawing.Point(12, 146);
-            this.butLIST2.Name = "butLIST2";
-            this.butLIST2.Size = new System.Drawing.Size(75, 23);
-            this.butLIST2.TabIndex = 6;
-            this.butLIST2.Text = "LIST2";
-            this.butLIST2.UseVisualStyleBackColor = true;
+            this.lblXLPath.AutoSize = true;
+            this.lblXLPath.Location = new System.Drawing.Point(132, 63);
+            this.lblXLPath.Name = "lblXLPath";
+            this.lblXLPath.Size = new System.Drawing.Size(139, 13);
+            this.lblXLPath.TabIndex = 4;
+            this.lblXLPath.Text = "Путь к файлу доработки";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 268);
-            this.Controls.Add(this.butLIST2);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.butLIST1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.lblXLPath);
+            this.Controls.Add(this.lblDBPath);
+            this.Controls.Add(this.btnXLPath);
+            this.Controls.Add(this.btnDBPath);
             this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "a p p W o r k";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -151,12 +131,10 @@ namespace appWork
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem выходAltF4ToolStripMenuItem;
-        private System.Windows.Forms.Button BtnCopy;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button butLIST1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button butLIST2;
+        private System.Windows.Forms.Button btnDBPath;
+        private System.Windows.Forms.Button btnXLPath;
+        private System.Windows.Forms.Label lblDBPath;
+        private System.Windows.Forms.Label lblXLPath;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,64 +14,44 @@ namespace appWork
 {
     public partial class Form1 : Form
     {
-        Excel.Workbook XLBook;
-        Excel.Worksheet XLLIST;
-        Excel.Range range;
+        public string DBPath;
+        public string XLPath;
+
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnDBPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Microsoft Access Database | *.mdb";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DBPath = ofd.FileName;
+                lblDBPath.Text = DBPath;
+            }
+        }
+
+        private void btnXLPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Microsoft Excel 97-2003 | *.xls";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                XLPath = ofd.FileName;
+                lblXLPath.Text = XLPath;
+            }
+        }
+
         private void выходAltF4ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void BtnCopy_Click(object sender, EventArgs e)
-        {
-            string fileName = @"D:\test\CopyMe.txt";
-            string PathPaste = @"D:\test\paste\CopyMe.txt";
-
-            System.IO.File.Copy(fileName, PathPaste);
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string path = @"D:\Dor\object\object.mdb";
-            StreamWriter sw = new StreamWriter(path);
-            //StreamWriter sw = new StreamWriter("test.txt", true, System.Text.Encoding.Default);
-            Console.WriteLine(textBox1.Text);
-            Console.WriteLine(path);
-            sw.WriteLine(textBox1.Text);
-            sw.Close();
-            
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //string path = @"D:\Dor\XL\tester.xlsx";
-            //StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
-            //string a = sr.ReadToEnd();
-            //Console.WriteLine(a);
-            //richTextBox1.Text = a;
-            //sr.Close();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
         }
     }
 }
