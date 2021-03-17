0731d3a908d85a91562a2ec6f6c2dc0158822c3c
Naked228 <57211122+Naked228@users.noreply.github.com>
2021-03-17 10:15:44 +0300

    old

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,13 +32,19 @@ namespace appWork
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.выходAltF4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.BtnCopy = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.butLIST1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.butLIST2 = new System.Windows.Forms.Button();
+            this.btnOpenXL = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnOpenDB = new System.Windows.Forms.Button();
+            this.BtnAddDB = new System.Windows.Forms.Button();
+            this.labelList = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.reg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.enabled = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -47,10 +53,9 @@ namespace appWork
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(778, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // файлToolStripMenuItem
             // 
@@ -67,80 +72,122 @@ namespace appWork
             this.выходAltF4ToolStripMenuItem.Text = "Выход Alt+F4";
             this.выходAltF4ToolStripMenuItem.Click += new System.EventHandler(this.выходAltF4ToolStripMenuItem_Click);
             // 
-            // BtnCopy
+            // btnOpenXL
             // 
-            this.BtnCopy.Location = new System.Drawing.Point(254, 53);
-            this.BtnCopy.Name = "BtnCopy";
-            this.BtnCopy.Size = new System.Drawing.Size(75, 23);
-            this.BtnCopy.TabIndex = 1;
-            this.BtnCopy.Text = "button1";
-            this.BtnCopy.UseVisualStyleBackColor = true;
-            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            this.btnOpenXL.Location = new System.Drawing.Point(12, 69);
+            this.btnOpenXL.Name = "btnOpenXL";
+            this.btnOpenXL.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenXL.TabIndex = 1;
+            this.btnOpenXL.Text = "Открыть XL";
+            this.btnOpenXL.UseVisualStyleBackColor = true;
+            this.btnOpenXL.Click += new System.EventHandler(this.btnOpenXL_Click);
             // 
-            // textBox1
+            // listView1
             // 
-            this.textBox1.Location = new System.Drawing.Point(254, 27);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.listView1.Location = new System.Drawing.Point(213, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(211, 283);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
-            // butLIST1
+            // btnOpenDB
             // 
-            this.butLIST1.Location = new System.Drawing.Point(12, 117);
-            this.butLIST1.Name = "butLIST1";
-            this.butLIST1.Size = new System.Drawing.Size(75, 23);
-            this.butLIST1.TabIndex = 3;
-            this.butLIST1.Text = "LIST1";
-            this.butLIST1.UseVisualStyleBackColor = true;
-            this.butLIST1.Click += new System.EventHandler(this.button2_Click);
+            this.btnOpenDB.Location = new System.Drawing.Point(12, 40);
+            this.btnOpenDB.Name = "btnOpenDB";
+            this.btnOpenDB.Size = new System.Drawing.Size(121, 23);
+            this.btnOpenDB.TabIndex = 5;
+            this.btnOpenDB.Text = "Открыть Object.mdb";
+            this.btnOpenDB.UseVisualStyleBackColor = true;
+            this.btnOpenDB.Click += new System.EventHandler(this.btnOpenDB_Click);
             // 
-            // button2
+            // BtnAddDB
             // 
-            this.button2.Location = new System.Drawing.Point(340, 53);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.BtnAddDB.Location = new System.Drawing.Point(12, 98);
+            this.BtnAddDB.Name = "BtnAddDB";
+            this.BtnAddDB.Size = new System.Drawing.Size(121, 23);
+            this.BtnAddDB.TabIndex = 6;
+            this.BtnAddDB.Text = "Добавить в Object";
+            this.BtnAddDB.UseVisualStyleBackColor = true;
+            this.BtnAddDB.Click += new System.EventHandler(this.BtnAddDB_Click);
             // 
-            // richTextBox1
+            // labelList
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(421, 27);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 5;
-            this.richTextBox1.Text = "";
+            this.labelList.AutoSize = true;
+            this.labelList.Location = new System.Drawing.Point(210, 24);
+            this.labelList.Name = "labelList";
+            this.labelList.Size = new System.Drawing.Size(35, 13);
+            this.labelList.TabIndex = 7;
+            this.labelList.Text = "label1";
+            this.labelList.Click += new System.EventHandler(this.labelList_Click);
             // 
-            // butLIST2
+            // button1
             // 
-            this.butLIST2.Location = new System.Drawing.Point(12, 146);
-            this.butLIST2.Name = "butLIST2";
-            this.butLIST2.Size = new System.Drawing.Size(75, 23);
-            this.butLIST2.TabIndex = 6;
-            this.butLIST2.Text = "LIST2";
-            this.butLIST2.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(12, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(185, 108);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.reg,
+            this.description,
+            this.enabled});
+            this.dataGridView1.Location = new System.Drawing.Point(444, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(317, 283);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // reg
+            // 
+            this.reg.HeaderText = "reg ";
+            this.reg.Name = "reg";
+            // 
+            // description
+            // 
+            this.description.HeaderText = "description";
+            this.description.Name = "description";
+            // 
+            // enabled
+            // 
+            this.enabled.HeaderText = "enable";
+            this.enabled.Name = "enabled";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 268);
-            this.Controls.Add(this.butLIST2);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.butLIST1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.BtnCopy);
+            this.ClientSize = new System.Drawing.Size(778, 344);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelList);
+            this.Controls.Add(this.BtnAddDB);
+            this.Controls.Add(this.btnOpenDB);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnOpenXL);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,12 +198,17 @@ namespace appWork
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem выходAltF4ToolStripMenuItem;
-        private System.Windows.Forms.Button BtnCopy;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button butLIST1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button butLIST2;
+        private System.Windows.Forms.Button btnOpenXL;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnOpenDB;
+        private System.Windows.Forms.Button BtnAddDB;
+        private System.Windows.Forms.Label labelList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn enabled;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -14,64 +15,138 @@ namespace appWork
 {
     public partial class Form1 : Form
     {
-        Excel.Workbook XLBook;
-        Excel.Worksheet XLLIST;
-        Excel.Range range;
+        Excel.Workbook xlBook; //Рабочая книга
+        Excel.Worksheet xlSheet; // Лист Excel
+        Excel.Range xlRange; // Диапазон
+        int iLastRow, iLastCol, cnt; // кол-Во строк, кол-во колонок, кол-во листов в Excel.
 
+        public string DBPath;
+        public string XLPath;
+        public static string connectDB;
+        private OleDbConnection newDBConnect;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void выходAltF4ToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnOpenDB_Click(object sender, EventArgs e)
         {
-            this.Close();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Microsoft Access Database | *.mdb";
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DBPath = ofd.FileName;
+                // lblDBPath.Text = DBPath;
+            }
         }
 
-        private void BtnCopy_Click(object sender, EventArgs e)
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string fileName = @"D:\test\CopyMe.txt";
-            string PathPaste = @"D:\test\paste\CopyMe.txt";
-
-            System.IO.File.Copy(fileName, PathPaste);
-
+            labelList.Text = "Выбран лист " + listView1.FocusedItem.Text;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void BtnAddDB_Click(object sender, EventArgs e)
+        {
+
+            //подключаем и открываем базу Object.mdb
+            connectDB = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DBPath;
+            newDBConnect = new OleDbConnection(connectDB);
+            newDBConnect.Open();
+            Excel.Application xlApp = new Excel.Application();
+            xlBook = xlApp.Workbooks.Open(XLPath);
+
+            string query = "INSERT INTO " + listView1.FocusedItem.Text + " VALUES "  " )"; // VALUES (dataArr)
+            OleDbCommand cmd = new OleDbCommand(query, newDBConnect);
+            cmd.ExecuteNonQuery();
+            // Вычитываем колонки из листа
+            //DataTable data_table = newDBConnect.GetSchema("Columns", new string[] { null, null, "listView1.FocusedItem.Text" });
+            //foreach (DataRow row in data_table.Rows)
+            //{
+            //    string TableName = row["COLUMN_NAME"].ToString();
+
+            //}
+            //DataTable dataArr = newDBConnect.GetSchema("Values", new string[] { null, null, "listView1.FocusedItem.Text" });
+            //foreach (DataRow row in dataArr.Rows)
+            //{
+            //    string TableName = row["Values"].ToString();
+            //    listBox1.Items.Add(TableName);
+            //}
+        }
+
+        private void labelList_Click(object sender, EventArgs e)
         {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string path = @"D:\Dor\object\object.mdb";
-            StreamWriter sw = new StreamWriter(path);
-            //StreamWriter sw = new StreamWriter("test.txt", true, System.Text.Encoding.Default);
-            Console.WriteLine(textBox1.Text);
-            Console.WriteLine(path);
-            sw.WriteLine(textBox1.Text);
-            sw.Close();
-            
-        }
+            //подключаем и открываем базу Object.mdb
+            connectDB = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + DBPath;
+            newDBConnect = new OleDbConnection(connectDB);
+            newDBConnect.Open();
+            // Водит все таблицы из базы
+            //DataTable tbls = newDBConnect.GetSchema("Tables", new string[] { null, null, null, "TABLE" }); //список всех таблиц
+            //foreach (DataRow row in tbls.Rows)
+            //{
+            //    string TableName = row["TABLE_NAME"].ToString();
+            //    listBox1.Items.Add(TableName);
+            //}
 
-        private void button2_Click(object sender, EventArgs e)
+            string val = "SELECT * FROM LIST5"; // строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(val, newDBConnect); // Команда
+            OleDbDataReader dbReader = dbCommand.ExecuteReader(); //Считываем данные
+
+            if (dbReader.HasRows == false)
+            {
+                MessageBox.Show("Данные не найдены так что иди нахуй");
+            }
+            else
+            {
+                while (dbReader.Read())
+                {
+                    dataGridView1.Rows.Add(dbReader ["reg"], dbReader["description"], dbReader["enabled"]);
+                }
+            }
+            Console.WriteLine(val);
+            dbReader.Close();
+            newDBConnect.Close();
+        }
+    
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //string path = @"D:\Dor\XL\tester.xlsx";
-            //StreamReader sr = new StreamReader(path, System.Text.Encoding.Default);
-            //string a = sr.ReadToEnd();
-            //Console.WriteLine(a);
-            //richTextBox1.Text = a;
-            //sr.Close();
+
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void выходAltF4ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+         private void btnOpenXL_Click(object sender, EventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Microsoft Excel 97-2003 | *.xls";
+            ofd.RestoreDirectory = true;
 
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                XLPath = ofd.FileName;
+                //lblXLPath.Text = XLPath;
+
+                //load xl
+                Excel.Application xlApp = new Excel.Application();
+                xlBook = xlApp.Workbooks.Open(XLPath);
+                cnt = xlApp.Sheets.Count;
+
+                for (int i = 1; i <= cnt; i++)
+                {
+                    listView1.Items.Add(xlApp.Sheets[i].Name);
+                }
+                xlBook.Close(true);
+                xlApp.Quit();
+            }
         }
+
     }
 }
